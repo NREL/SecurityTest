1112556d769f89f9705daef625d763983358b63d
Nathan Meadows <nathan@negativenull.net>
2021-03-09 08:49:15 -0700

    testing

--- insecure.py
+++ insecure.py
@@ -5,4 +5,9 @@ from urllib.request import urlopen
 obj = pickle.loads(urlopen(sys.argv[1]).read())
 print(obj)
 
+
+with open('serial', 'r') as f:
+    pickle.loads(f.read())
+
+
 #this is a test code snippet that should trigger security problems
